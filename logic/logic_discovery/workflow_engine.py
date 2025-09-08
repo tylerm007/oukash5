6d6ffefebc8d7a6ad6da6919cc6f674f662f6d4b
tyler band <tylerm007@gmial.com>
2025-09-08 15:04:35 -0400

    fix docker for Ontimize and add post script engine

--- logic/logic_discovery/workflow_engine.py
+++ logic/logic_discovery/workflow_engine.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from math import log
+from operator import call
 from database.models import TaskInstance
 from integration.workflow import python_engine
 from flask import app, request, jsonify, session
@@ -26,7 +27,19 @@ def get_application(application_id):
         return None, jsonify({"success": False, "message": f"No application found with ApplicationID {application_id}"}), 404
     return application
 
-def test(row: models.TaskInstance, old_row: models.TaskInstance, logic_row: LogicRow):
+def call_script_engine_pre(row: models.TaskInstance, old_row: models.TaskInstance, logic_row: LogicRow):
+    task_def = row.TaskDef
+    script = task_def.PreScriptJson or ''
+    logic_row.log(f'PreScriptJson: {script}')
+    call_script_engine(row, old_row, logic_row, script)
+
+def call_script_engine_post(row: models.TaskInstance, old_row: models.TaskInstance, logic_row: LogicRow):
+    task_def = row.TaskDef
+    script = task_def.PostScriptJson or ''
+    logic_row.log(f'PostScriptJson: {script}')
+    call_script_engine(row, old_row, logic_row, script)
+
+def call_script_engine(row: models.TaskInstance, old_row: models.TaskInstance, logic_row: LogicRow, script: str):
 
     if logic_row.ins_upd_dlt == 'upd':  
         task_id = row.TaskInstanceId
@@ -34,9 +47,8 @@ def test(row: models.TaskInstance, old_row: models.TaskInstance, logic_row: Logi
             task_def = row.TaskDef
             application_id = row.Stage.ProcessInstance.ApplicationId
             se = python_engine.PythonScriptEngine()
-            script = task_def.PreScriptJson or ''
-            context = {"data": row.to_dict(),"application_id": application_id}
-            external_context = {"get_application": get_application}
+            context = {"data": row.to_dict(),"application_id": application_id, "task": row}
+            external_context = {"get_application": get_application,"models":models,"session":session,"db":db,"app_logger":app_logger,"Args":Args,"Config":Config,"datetime":datetime,"Decimal":Decimal,"logic_row": logic_row}
             r = se.execute(script=script, task=context, external_context=external_context)
             if r:
                 app_logger.info(f'Script executed successfully for task_id {task_id}')
@@ -50,4 +62,5 @@ def declare_logic():
     pass
 
     # add logic here if needed
-    Rule.row_event(on_class=models.TaskInstance,calling=test)
+    Rule.row_event(on_class=models.TaskInstance,calling=call_script_engine_pre)
+    Rule.after_flush_row_event(on_class=models.TaskInstance, calling=call_script_engine_post)
